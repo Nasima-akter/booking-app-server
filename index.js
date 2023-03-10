ac8d435853ba43b78568db328239b0ec265eb1f6
Nasima-akter <nasimatpi@gmail.com>
2023-03-10 17:20:26 +0600

    conditional sign out

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -19,6 +20,7 @@ async function run() {
     try {
         const appointmentOptionCollection = client.db('bookingApp').collection('appointmentOption');
         const bookingsCollection = client.db('bookingApp').collection('bookings');
+        const usersCollection = client.db('bookingApp').collection('users');
 
         // use Aggregate to query multiple collection and then merge date.
         app.get('/appointmentOption', async (req, res) => {
@@ -36,16 +38,84 @@ async function run() {
                 const bookedSlots = optionBooked.map(book => book.slot)
                 const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
                 option.slots = remainingSlots;
-                // console.log(date, option.name, remainingSlots.length);
             })
             res.send(options);
-
         });
+
+        app.get('/v2/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        })
+        app.get('/bookings', async(req, res) =>{
+            const email = req.query.email;
+            console.log(email);
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
 
         app.post('/bookings', async(req, res) =>{
             const booking = req.body;
-            // console.log(booking);
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment 
+            }
+            const alreadyBooked  = await bookingsCollection.find(query).toArray();
+            
+            if (alreadyBooked.length){
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({acknowledged: false, message})
+            }
+            
             const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
     }
